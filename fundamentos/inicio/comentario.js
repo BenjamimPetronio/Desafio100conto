4ed873546fbf10785009a30074c0b1a7fd6a611b
RicardoRR29 <r3ifes@gmail.com>
2020-12-08 20:11:04 -0300

    adicionando exercicios do dia 07/12

---
+++ fundamentos/inicio/comentario.js
@@ -0,0 +1,4 @@
+//console.log é usado para exibir algo na tela do compuitador
+console.log("pitbull");
+console.log("pinscher");
+console.log("maltes");//cao,peludo e branco
--- fundamentos/inicio/primeiro.js
+++ fundamentos/inicio/primeiro.js
@@ -1,4 +1,3 @@
-//console.log é usado para exibir algo na tela do compuitador
 console.log("pitbull");
 console.log("pinscher");
-console.log("maltes");//cao,peludo e branco
+console.log("maltes");
